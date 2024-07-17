839fcbccb0686a83b0c1fa6ab379878b063f7288
krushnaT <krushnaT@LAPTOP-6ETMCADA>
2024-07-17 10:00:22 +0530

    Ashok write login test case

---
+++ src/main/java/Tutorial2/Demo2.java
@@ -0,0 +1,12 @@
+package Tutorial2;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("login test case written by Ashok Automation tester  ");
+		
+	}
+
+}
